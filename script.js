5e653eef03b87cdc7b6c4a982c0f8bee6d4c44e6
okssferl <ferleivapozo01@gmail.com>
2025-07-13 15:53:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,80 @@
-const ramos = {
+const ramosPorSemestre = {
+  "I Semestre": [
+    "Tecnología y Sistemas de Información",
+    "Métodos Matemáticos I",
+    "Introducción a la Economía",
+    "Gestión y Empresas",
+    "Inglés"
+  ],
+  "II Semestre": [
+    "Aplicaciones de Manejo de Datos",
+    "Métodos Matemáticos II",
+    "Introducción a la Microeconomía",
+    "Contabilidad I",
+    "Inglés I",
+    "Entorno Social y Profesional"
+  ],
+  "III Semestre": [
+    "Estadística I",
+    "Métodos Matemáticos III",
+    "Introducción a la Macroeconomía",
+    "Contabilidad II",
+    "Inglés II",
+    "Libre I"
+  ],
+  "IV Semestre": [
+    "Estadística II",
+    "Introducción a las Finanzas",
+    "Auditoría I",
+    "Fundamentos de Costos",
+    "Inglés III",
+    "Taller de Integración Profesional"
+  ],
+  "V Semestre": [
+    "Métodos Cuantitativos",
+    "Tributación I",
+    "Análisis Contable",
+    "Costos y Presupuestos",
+    "Inglés IV",
+    "Entorno Legal"
+  ],
+  "VI Semestre": [
+    "Análisis y Planificación Financiera",
+    "Tributación II",
+    "Auditoría II",
+    "Control de Gestión",
+    "Libre II",
+    "Libre III"
+  ],
+  "VII Semestre": [
+    "Gestión Financiera Corporativa",
+    "Tributación III",
+    "Auditoría III",
+    "Costos Avanzados",
+    "Contabilidad Avanzada"
+  ],
+  "VIII Semestre": [
+    "Tecnología y Base de datos para Auditoría",
+    "Gestión de Procesos de Negocios",
+    "Tributación IV",
+    "Tributación V",
+    "Taller de Contabilidad y Auditoría"
+  ],
+  "IX Semestre": [
+    "Tributación VI",
+    "Auditoría IV",
+    "Contabilidad para las Decisiones",
+    "Práctica Profesional I"
+  ],
+  "X Semestre": [
+    "Electivo I",
+    "Electivo II",
+    "Electivo III",
+    "Práctica Profesional II"
+  ]
+};
+
+const requisitos = {
   "Tecnología y Sistemas de Información": [],
   "Métodos Matemáticos I": [],
   "Introducción a la Economía": [],
@@ -56,52 +132,4 @@ const ramos = {
 
 const estadoRamos = {};
 
-const contenedor = document.getElementById("malla");
-
-function crearMalla() {
-  contenedor.innerHTML = "";
-  for (const [ramo, requisitos] of Object.entries(ramos)) {
-    const div = document.createElement("div");
-    div.className = "ramo";
-
-    if (!estaDesbloqueado(ramo)) {
-      div.classList.add("bloqueado");
-    }
-
-    if (estadoRamos[ramo]) {
-      div.classList.add("aprobado");
-    }
-
-    div.innerText = ramo;
-    div.onclick = () => {
-      if (!estaDesbloqueado(ramo)) return;
-
-      estadoRamos[ramo] = !estadoRamos[ramo];
-      actualizarMalla();
-    };
-
-    contenedor.appendChild(div);
-  }
-}
-
-function estaDesbloqueado(ramo) {
-  const requisitos = ramos[ramo];
-  return requisitos.every(req => estadoRamos[req]);
-}
-
-function actualizarMalla() {
-  crearMalla();
-  guardarEstado();
-}
-
-function guardarEstado() {
-  localStorage.setItem("estadoRamos", JSON.stringify(estadoRamos));
-}
-
-function cargarEstado() {
-  const guardado = JSON.parse(localStorage.getItem("estadoRamos") || "{}");
-  Object.assign(estadoRamos, guardado);
-}
-
-cargarEstado();
-crearMalla();
+const contenedor = document.getElementByI
